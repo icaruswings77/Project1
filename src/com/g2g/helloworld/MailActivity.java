bbdc1242a9ba938477bf5bea12900a76e66951a4
icaruswings <snowthing@naver.com>
2012-10-21 18:55:55 +0900

    따라하기

--- src/com/g2g/helloworld/MailActivity.java
+++ src/com/g2g/helloworld/MailActivity.java
@@ -46,7 +46,7 @@ public class MailActivity extends Activity {
         send.setOnClickListener(new View.OnClickListener() { 
         	
             public void onClick(View view) { 
-            GMailSender sender = new GMailSender("icaruswings77@gmail.com","snowball77"); // SUBSTITUTE HERE                   
+            GMailSender sender = new GMailSender("icaruswings77@gmail.com","pass"); // SUBSTITUTE HERE                   
                 try { 
                     sender.sendMail( 
                             "메일제목 !!",   					//subject.getText().toString(),  
@@ -62,79 +62,4 @@ public class MailActivity extends Activity {
 
     }
     
-    public void GMailSender(String user, String password) { 
-        this.user = user; 
-        this.password = password; 
- 
-        Properties props = new Properties();  
-        props.setProperty("mail.transport.protocol", "smtp");  
-        props.setProperty("mail.host", mailhost);  
-        props.put("mail.smtp.auth", "true");  
-        props.put("mail.smtp.port", "465");  
-        props.put("mail.smtp.socketFactory.port", "465");  
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");  
-        props.put("mail.smtp.socketFactory.fallback", "false");  
-        props.setProperty("mail.smtp.quitwait", "false");  
- 
-        //session = Session.getDefaultInstance(props, this); 
-        
-    } 
-
-    protected PasswordAuthentication getPasswordAuthentication() { 
-        return new PasswordAuthentication(user, password); 
-    } 
- 
-    public synchronized void sendMail(String subject, String body, String sender, String recipients) throws Exception { 
-        
-    	MimeMessage message = new MimeMessage(session); 
-        DataHandler handler = new DataHandler(new ByteArrayDataSource(body.getBytes(), "text/plain")); 
-        message.setSender(new InternetAddress(sender)); 
-        message.setSubject(subject); 
-        message.setDataHandler(handler); 
-        
-        if (recipients.indexOf(',') > 0) 
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients)); 
-        else 
-            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipients)); 
-        Transport.send(message); 
-    } 
- 
-    public class ByteArrayDataSource implements DataSource { 
-        private byte[] data; 
-        private String type; 
- 
-        public ByteArrayDataSource(byte[] data, String type) { 
-            super(); 
-            this.data = data; 
-            this.type = type; 
-        } 
- 
-        public ByteArrayDataSource(byte[] data) { 
-            super(); 
-            this.data = data; 
-        } 
- 
-        public void setType(String type) { 
-            this.type = type; 
-        } 
- 
-        public String getContentType() { 
-            if (type == null) 
-                return "application/octet-stream"; 
-            else 
-                return type; 
-        } 
- 
-        public InputStream getInputStream() throws IOException { 
-            return new ByteArrayInputStream(data); 
-        } 
- 
-        public String getName() { 
-            return "ByteArrayDataSource"; 
-        } 
- 
-        public OutputStream getOutputStream() throws IOException { 
-            throw new IOException("Not Supported"); 
-        } 
-    }     
 }    
